3c53ebd5c70e37cf51ddee4258d65bf0c036eaf5
edezuraud <edezuraud@gedicom.fr>
2023-03-12 19:10:45 +0100

    Add post to api

--- index.js
+++ index.js
@@ -9,17 +9,43 @@ import {
 const app = express()
 const port = process.env.PORT || 5566
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"),
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"),
-    next()
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"),
+        next()
 })
 
 app.use(bodyParser.json({ type: 'application/*+json' }))
 
+app.post('/api', async (req, res) => {
+    try {
+        let theme = '';
+
+        const resObj = {
+            title: '',
+            color: '',
+            slides: []
+        }
+
+        if (req.body.prompt) {
+            theme = req.body.prompt;
+        }
+
+        if (theme) {
+            const lang = getLang(theme);
+            resObj.color = await getPrimaryColor(theme, false);
+            resObj.title = await getTitle(theme, lang, true);
+            resObj.slides = await getSlides(resObj.title, lang, 5, true);
+        }
+
+        res.json(resObj);
+    } catch (error) {
+        throw error;
+    }
+})
+
 app.get('/api', async (req, res) => {
     try {
-        const url = req.body;
         const query = req.query;
         let theme = '';
 
@@ -29,13 +55,7 @@ app.get('/api', async (req, res) => {
             slides: []
         }
 
-        // console.log(process.env.OPENAI_API_KEY);
-
-        if (req.body.prompt) {
-            theme = req.body.prompt;
-        } else if (query.prompt) {
-            theme = query.prompt;
-        }
+        theme = query.prompt;
 
         if (theme) {
             const lang = getLang(theme);
