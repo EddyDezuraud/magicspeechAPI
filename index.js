e333d5e559f4fbd188bda7c16959454a60474509
edezuraud <edezuraud@gedicom.fr>
2023-03-12 19:02:48 +0100

    Allow cors origin

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ import {
 const app = express()
 const port = process.env.PORT || 5566
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"),
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"),
+    next()
+})
+
 app.use(bodyParser.json({ type: 'application/*+json' }))
 
 app.get('/api', async (req, res) => {
